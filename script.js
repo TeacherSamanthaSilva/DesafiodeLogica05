98e2c0c62c246bf2526534855c1a9d94823ffb84
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-25 23:27:42 -0300

    questão dois
    Nesse commit fiz a segunda questão

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Crie um array que receba 5 itens e exiba no console.
 
 let princesasDisney = ["Bela", "Aurora", "Merinda", "Ariel", "Moana"];
-console.log(princesasDisney[0]) 
-console.log(princesasDisney[1]) 
-console.log(princesasDisney[2]) 
-console.log(princesasDisney[3]) 
-console.log(princesasDisney[4])
+console.log(princesasDisney[0]) ;
+console.log(princesasDisney[1]) ;
+console.log(princesasDisney[2]) ;
+console.log(princesasDisney[3]) ;
+console.log(princesasDisney[4]);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+let princesasDisney = ["Bela", "Aurora", "Merinda", "Ariel", "Moana"];
+princesasDisney.unshift("Jasmin");
+console.log(princesasDisney);
