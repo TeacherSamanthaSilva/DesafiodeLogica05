c3b6650f5715148869b47a19c500ad6450c59e42
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-25 23:24:57 -0300

    primeira questão
    Nesse commit fiz a primeira questão

---
+++ script.js
@@ -0,0 +1,8 @@
+// Crie um array que receba 5 itens e exiba no console.
+
+let princesasDisney = ["Bela", "Aurora", "Merinda", "Ariel", "Moana"];
+console.log(princesasDisney[0]) 
+console.log(princesasDisney[1]) 
+console.log(princesasDisney[2]) 
+console.log(princesasDisney[3]) 
+console.log(princesasDisney[4])
